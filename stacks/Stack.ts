876326b6f71cbcd0d94554cab2a680f11650efea
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-13 00:05:11 -0700

    frontend

--- stacks/Stack.ts
+++ stacks/Stack.ts
@@ -1,7 +1,13 @@
-import { StackContext, GraphQLApi } from "@serverless-stack/resources";
+import {
+  StackContext,
+  GraphQLApi,
+  ReactStaticSite,
+} from "@serverless-stack/resources";
+
+const { DOMAIN, SUBDOMAIN } = process.env;
 
 export function Stack({ stack }: StackContext) {
-  const api = new GraphQLApi(stack, "GraphqlApi", {
+  const graphql = new GraphQLApi(stack, "graphql", {
     server: {
       handler: "functions/lambda.handler",
       bundle: {
@@ -9,7 +15,25 @@ export function Stack({ stack }: StackContext) {
       },
     },
   });
+
+  const site = new ReactStaticSite(stack, "site", {
+    path: "frontend",
+    environment: {
+      REACT_APP_REGION: stack.region,
+      REACT_APP_API_URL: graphql.url,
+      // REACT_APP_USER_POOL_ID: auth.userPoolId,
+      // REACT_APP_USER_POOL_CLIENT_ID: auth.userPoolClientId,
+      // REACT_APP_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId || "",
+    },
+    customDomain: {
+      domainName: `${SUBDOMAIN}.${DOMAIN}`,
+      domainAlias: `www.${SUBDOMAIN}.${DOMAIN}`,
+      hostedZone: `${DOMAIN}`,
+    },
+  });
+
   stack.addOutputs({
-    ApiEndpoint: api.url,
+    GraphqlApiEndpoint: graphql.url,
+    SiteUrl: site.url,
   });
 }
