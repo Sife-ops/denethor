ca2233c7fed60fa49884e6c196f45b14c793c0d5
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-12 23:13:44 -0700

    graphql

---
+++ stacks/Stack.ts
@@ -0,0 +1,15 @@
+import { StackContext, Api, GraphQLApi } from "@serverless-stack/resources";
+
+export function Stack({ stack }: StackContext) {
+  const api = new GraphQLApi(stack, "GraphqlApi", {
+    server: {
+      handler: "functions/lambda.handler",
+      bundle: {
+        minify: false,
+      },
+    },
+  });
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+}
