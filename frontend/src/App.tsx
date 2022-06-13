4e0ba7a4ebb2913b31ba6c5c5a37b12d4f0342a2
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-13 00:47:37 -0700

    test query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  React.useEffect(() => {
+    console.log(process.env.REACT_APP_API_URL);
+
+    fetch("https://6aeocxypzd.execute-api.us-east-1.amazonaws.com", {
+      method: "POST",
+      headers: {
+        // Authorization: `bearer ${REACT_APP_GITHUB_AUTH_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // query: text,
+        // variables,
+        query: `
+          query HelloQuery {
+            hello
+          }
+        `,
+      }),
+    }).then(async (res) => {
+      //
+      const json = await res.json();
+      console.log(json);
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* // */}
+      hello
     </div>
   );
 }
