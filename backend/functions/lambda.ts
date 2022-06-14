5e7f5f9110430c7d4a924950e0f208681a6a89e1
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-14 15:34:48 -0700

    context userId

--- backend/functions/lambda.ts
+++ backend/functions/lambda.ts
@@ -1,3 +1,4 @@
+import jwt_decode, { JwtPayload } from "jwt-decode";
 import { ApolloServer, gql } from "apollo-server-lambda";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
@@ -11,13 +12,40 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    hello: (_: any, __: any, context: any, info: any) => {
+      console.log(context.userId);
+      return `Hello world!`;
+    },
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({
+    event,
+    // express,
+    // context,
+  }): {
+    userId: string;
+    // event: any;
+    // context: any;
+  } => {
+    const decoded = jwt_decode<JwtPayload>(event.headers.authorization);
+
+    if (!decoded.sub) {
+      throw new Error("userId undefined");
+    }
+
+    return {
+      userId: decoded.sub,
+      // headers: event.headers,
+      // functionName: context.functionName,
+      // expressRequest: express.req,
+      // event,
+      // context,
+    };
+  },
 
   // By default, the GraphQL Playground interface and GraphQL introspection
   // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
