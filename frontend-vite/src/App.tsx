ad35e3a58135435d426100cb917fb1a0c2fd124a
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-06-17 04:14:39 -0700

    amplify

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,6 +1,33 @@
 import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Amplify } from "aws-amplify";
+
+// todo: move to file
+const env = {
+  apiGateway: {
+    REGION: import.meta.env.VITE_REGION,
+    URL: import.meta.env.VITE_API_URL,
+  },
+  cognito: {
+    REGION: import.meta.env.VITE_REGION,
+    USER_POOL_ID: import.meta.env.VITE_USER_POOL_ID,
+    USER_POOL_CLIENT_ID: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+    IDENTITY_POOL_ID: import.meta.env.VITE_IDENTITY_POOL_ID,
+  },
+};
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: env.cognito.REGION,
+    userPoolId: env.cognito.USER_POOL_ID,
+    identityPoolId: env.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: env.cognito.USER_POOL_CLIENT_ID,
+  },
+});
+
+console.log(env);
 
 function App() {
   const [count, setCount] = useState(0);
